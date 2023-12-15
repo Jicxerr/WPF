1037838bf4e628d42dea8908f539213f9d0154be
Jicxerr <jtj9928@students.uc-bcf.edu.ph>
2023-12-15 11:41:17 +0800

    project

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Student_Information_Entry;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Student_Information_Entry;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OpenEntryPage_Click(object sender, RoutedEventArgs e)
+    {
+        EntryPage entryPage = new EntryPage();
+        entryPage.Show();
+        this.Close();
+    }
+
+    private void OpenDevelopersPage_Click(object sender, RoutedEventArgs e)
+    {
+        DevelopersPage developersPage = new DevelopersPage();
+        developersPage.Show();
+        this.Close();
+    }
+}
+
+internal class DevelopersPage
+{
+    public DevelopersPage()
+    {
+    }
+
+    internal void Show()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class EntryPage
+{
+    public EntryPage()
+    {
+    }
+
+    internal void Show()
+    {
+        throw new NotImplementedException();
+    }
+}
